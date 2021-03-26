b323419da1bac31c5a6fc4c88ced28fa3c18c180
Jose Ochoa <joseo@Joses-MBP.attlocal.net>
2021-03-26 14:29:10 -0400

    cross origin

--- Quicklist/src/main/java/com/project2/controller/UserControllerImpl.java
+++ Quicklist/src/main/java/com/project2/controller/UserControllerImpl.java
@@ -2,7 +2,7 @@ package com.project2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +18,7 @@ import com.project2.models.Users;
 import com.project2.service.UserService;
 
 @Controller("userController")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserControllerImpl implements UserController{
 	
 	@Autowired
